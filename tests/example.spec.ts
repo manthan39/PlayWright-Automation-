309a09430dac78868c67bb0f9b088035ea3bd37e
mbhatiya <mbhatiya@mobiquityinc.com>
2022-03-14 16:57:07 +0530

    Added sample code to playwright project

---
+++ tests/example.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('basic test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.screenshot({path:'screnshot.png',fullPage:true})
+  await page.click('text=Get started')
+  const title = page.locator('.navbar__inner .navbar__title');
+  await expect(title).toHaveText('Playwright');
+});
