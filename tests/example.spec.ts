241b21cfc3bdc39f1b940f78afa378d1169aa67c
ManthanBhatiya <bhatiyamanthan@gmail.com>
2025-09-24 11:47:30 +0530

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,10 @@
 import { test, expect } from '@playwright/test';
 
+/**
+  * @author John Doe
+  * @createdDate 2025-08-23
+  * @updatedDate 2025-10-04
+  */ 
 test('basic test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await page.screenshot({path:'screnshot.png',fullPage:true})
